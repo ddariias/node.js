9ba159aa2f3b5406cdcf7847d327c1171d97959e
Dasha <dashadidyk41@gmail.com>
2024-09-08 13:29:01 +0300

    commit hw1.2

--- index.js
+++ index.js
@@ -13,35 +13,32 @@ const proxy = async () => {
     server.listen(3000);
 
 
-  await fsPromises.mkdir(path.join(__dirname, 'baseFolder'), {recursive:true})
+    await fsPromises.mkdir(path.join(__dirname, 'baseFolder'), {recursive: true})
     const pathDirectory = path.join(__dirname, 'baseFolder')
 
-    for (let i = 1; i <= 5; i++) {
-        await fsPromises.mkdir(path.join(pathDirectory, `new folder${i}`), {recursive:true})
-    }
 
-    for (let i = 1; i <= 5; i++){
+    for (let i = 1; i <= 5; i++) {
+        await fsPromises.mkdir(path.join(pathDirectory, `new folder${i}`), {recursive: true})
+
         const pathFile = path.join(pathDirectory, `new folder${i}`)
-        const file = path.join(pathFile, `test${i}.txt`)
-        for(let i = 1; i <= 5; i++){
-            await fsPromises.writeFile(file, 'Hello!')
-        }
+
         console.log(`${pathFile}`)
         const stat = await fsPromises.stat(`${pathFile}`)
         console.log(stat.isDirectory());
         console.log(stat.isFile());
 
-        console.log(`${file}`)
-        const statFile = await  fsPromises.stat(`${file}`)
-        console.log(statFile.isDirectory());
-        console.log(statFile.isFile());
-    }
+        for (let p = 1; p <= 5; p++) {
+            const file = path.join(pathFile, `test${p}.txt`)
+            await fsPromises.writeFile(file, 'Hello!')
 
+            console.log(`${file}`)
+            const statFile = await  fsPromises.stat(`${file}`)
+            console.log(statFile.isDirectory());
+            console.log(statFile.isFile());
+        }
+    }
     console.log(__dirname);
     console.log(pathDirectory)
-
-
-
 
 }
 void proxy();
