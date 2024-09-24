6cfae29370f6958d0f5b04dfbed6c22a0fec189f
Dasha <dashadidyk41@gmail.com>
2024-09-24 21:32:24 +0300

    hw5

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -3,26 +3,14 @@ import { Router } from "express";
 import { userController } from "../controllers/user.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { userMiddleware } from "../middleware/user.middleware";
-import { UserValidator } from "../validators/user.validator";
 
 const router = Router();
 
 router.get("/", userController.getAll);
 
 router.get("/me", authMiddleware.checkRefreshToken, userController.getMe);
-router.put(
-  "/me",
-  authMiddleware.checkAccessToken,
-  userMiddleware.isIdValid("userId"),
-  userMiddleware.isBodyValid(UserValidator.update),
-  userController.updateMe,
-);
-router.delete(
-  "/me",
-  authMiddleware.checkAccessToken,
-  userMiddleware.isIdValid("userId"),
-  userController.deleteMe,
-);
+router.put("/me", authMiddleware.checkAccessToken, userController.updateMe);
+router.delete("/me", authMiddleware.checkAccessToken, userController.deleteMe);
 router.get(
   "/:userId",
   userMiddleware.isIdValid("userId"),
